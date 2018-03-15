f0c8def63f4d9159919539a85b4259e4bd5f3493
Irene <25118334+isteves@users.noreply.github.com>
2018-03-15 08:57:39 -0700

    Start package

---
+++ R/functions.R
@@ -0,0 +1,29 @@
+#' A function
+#' 
+#' This function adds one to the input.
+#' 
+#' @param x (numeric) input number
+#' 
+#' @keywords add
+#' @export
+#' @examples 
+#' function1(2) #becomes 3
+
+function1 <- function(x){
+  x + 1
+}
+
+#' Another function
+#' 
+#' This function adds two to the input
+#' 
+#' @param x (numeric) input number
+#' 
+#' @keywords add
+#' @export
+#' @examples 
+#' function2(2) #becomes 4
+
+function2 <- function(x){
+  x + 2
+}
